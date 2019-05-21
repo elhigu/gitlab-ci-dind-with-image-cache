8f5f05b61509dab7e8ea9c6c4be5f881602ca31b
Mikael Lepistö <mikael.lepisto@vincit.fi>
2019-05-22 00:55:07 +0300

    Custom dind image with loopback ext4 filesystem for docker images
    This image can be saved with docker commit and then re-ran as other containers with pre pulled images.

---
+++ .gitlab-custom-dind/dockerd-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+# for some reason /etc/fstab entry didn't work
+mount -t ext4 -o loop /var-lib-docker.loopback.ext4 /var-lib-docker
+
+# no arguments passed
+# or first arg is `-f` or `--some-option`
+if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
+	# add our default arguments
+	set -- dockerd \
+    --data-root=/var-lib-docker \
+		--host=unix:///var/run/docker.sock \
+		--host=tcp://0.0.0.0:2375 \
+		"$@"
+fi
+
+if [ "$1" = 'dockerd' ]; then
+	if [ -x '/usr/local/bin/dind' ]; then
+		# if we have the (mostly defunct now) Docker-in-Docker wrapper script, use it
+		set -- '/usr/local/bin/dind' "$@"
+	fi
+
+	# explicitly remove Docker's default PID file to ensure that it can start properly if it was stopped uncleanly (and thus didn't clean up the PID file)
+	find /run /var/run -iname 'docker*.pid' -delete
+fi
+
+exec "$@"
